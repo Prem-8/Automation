479f93e6f13c5bb4981e2559b9d63642eb4c7d4e
home <home@DESKTOP-21J6DGF>
2021-05-09 09:18:48 -0700

    pushing

--- Remp_Project/src/test/java/com/crm/vtiger/TestCaseWithPom/SampleTest.java
+++ Remp_Project/src/test/java/com/crm/vtiger/TestCaseWithPom/SampleTest.java
@@ -1,39 +1,13 @@
 package com.crm.vtiger.TestCaseWithPom;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SampleTest {
-	
-	@BeforeClass
-	public void configBC(){
-		System.out.println("ececute before class");
-	}
-	
-	@BeforeMethod
-	public void configBM() {
-		System.out.println("execute Before test");
-	}
-	
+public class SampleTest 
+{
 	@Test
-	public void creatOrg(){
-		System.out.println("execute creatOrg test");
-	}
-	@Test
-	public void createOrgWithIndutries(){
-		System.out.println("execute createOrgWithIndutries test");
-	}
-	
-	@AfterMethod
-	public void configAM() {
-		System.out.println("execute after test");
-	}
-	@AfterClass
-	public void configAC(){
-		System.out.println("ececute after class");
+	public void prem()
+	{
+		System.out.println("Haiii Prem");
 	}
 	
 
